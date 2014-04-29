d66b88b11b4d6e85bbba0ab4eb2b18e6f4dc4220
CJ Davies <cj@cjdavies.org>
2014-04-29 17:34:03 +0100

    New switching controls, more logging
    Added faded hard switching (using linear interpolation), added A, B &
    right trigger values to logging, log file is now auto named to the
    current timestamp at beginning of execution.

--- Assets/Scripts/DataLogger.cs
+++ Assets/Scripts/DataLogger.cs
@@ -19,17 +19,20 @@ public class DataLogger : MonoBehaviour {
 
     Vector3 position;
     Quaternion leftRotation, rightRotation;
-    float leftWebCamTextureOpacity, rightWebCamTextureOpacity;
+    float leftWebCamTextureOpacity, rightWebCamTextureOpacity, rTrigger;
+    bool A, B;
 
     //for calculating FPS
     float deltaTime = 0.0f;
     float fps;
 
-    // <timestamp> <position (vector3)> <orientation[L] (quaternion)> <orientation[L] (quaternion)> <webcam opacity[L]> <webcam opacity[R]> <FPS>
-    // 28/04/2014 15:53:55:332 (-21.5, 4.7, -8.6) (0.0, 0.7, 0.0, 0.7) (0.0, 0.7, 0.0, 0.7) 0 0 54.68808
+    // <timestamp> <position (vector3)> <orientation[L] (quaternion)> <orientation[L] (quaternion)> <webcam opacity[L]> <webcam opacity[R]> <FPS> <A> <B> <rTrigger>
+    // 29-04-2014 16-45-11-654 (1.8, 1.0, -8.3) (-0.1, -0.7, -0.1, 0.7) (-0.1, -0.7, -0.1, 0.7) 1 1 38.38756 False False 0
     string lines;
 
     void Start () {
+        timestamp = DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss-fff");
+        filePath = "Mirrorshades " + timestamp + ".log";
 	    player = GameObject.Find("OVRPlayerController2");
         leftCam = GameObject.Find("CameraLeft");
         rightCam = GameObject.Find("CameraRight");
@@ -39,7 +42,7 @@ public class DataLogger : MonoBehaviour {
 	
 	void Update () {
 
-        timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss:fff");
+        timestamp = DateTime.Now.ToString("dd-MM-yyyy HH-mm-ss-fff");
 
         //calculate FPS
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
@@ -51,9 +54,12 @@ public class DataLogger : MonoBehaviour {
         rightRotation = rightCam.transform.rotation;
         leftWebCamTextureOpacity = leftWebCamTexture.renderer.material.color.a;
         rightWebCamTextureOpacity = rightWebCamTexture.renderer.material.color.a;
+        A = player.GetComponent<XboxCameras>().A;
+        B = player.GetComponent<XboxCameras>().B;
+        rTrigger = player.GetComponent<XboxCameras>().rTrigger;
 
         lines += ((timestamp + " " + position + " " + leftRotation + " " + rightRotation + " " + leftWebCamTextureOpacity
-             + " " + rightWebCamTextureOpacity + " " + fps + "\n"));
+             + " " + rightWebCamTextureOpacity + " " + fps + " " + A + " " + B + " " + rTrigger + "\n"));
 
         //on each certain multiple of frames, write the contents of lines out to a file & then clear lines
         if (count == 100) {
--- Assets/Scripts/XboxCameras.cs
+++ Assets/Scripts/XboxCameras.cs
@@ -3,50 +3,77 @@ using System.Collections;
 
 public class XboxCameras : MonoBehaviour {
 
+    public bool A { get; set; }
+    public bool B { get; set; }
+    public float rTrigger { get; set; }
+
+    bool Btog;
+
+    Color originalColour;
+
+    GameObject CameraLeft, CameraRight, WebcamLeft, WebcamRight;
+
 	// Use this for initialization
 	void Start () {
-	
+        //CameraLeft = GameObject.Find("CameraLeft");
+        //CameraRight = GameObject.Find("CameraRight");
+        
+        WebcamLeft = GameObject.Find("WebcamLeft");
+        WebcamRight = GameObject.Find("WebcamRight");
+
+        GameObject.Find("CameraLeft").camera.cullingMask = ((1 << 30) | (1 << 0)); //1 << 30;
+        GameObject.Find("CameraRight").camera.cullingMask = ((1 << 31) | (1 << 0)); //1 << 31;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        GameObject.Find("CameraLeft").camera.cullingMask = ((1 << 30) | (1 << 0)); //1 << 30;
-        GameObject.Find("CameraRight").camera.cullingMask = ((1 << 31) | (1 << 0)); //1 << 31;
-        
-        //GameObject.Find("CameraLeft").camera.cullingMask = 1 << 0;
-        //GameObject.Find("CameraRight").camera.cullingMask = 1 << 0;
-        
-        Color originalColour;
+        Debug.Log("Btog: " + Btog);
 
-        if(Input.GetButton("Fire1")) {
-            originalColour = GameObject.Find("WebcamLeft").renderer.material.color;
-            GameObject.Find("WebcamLeft").renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 0.0f);
+        A = Input.GetButton("Fire1");
+        B = Input.GetButton("Fire2");
+        rTrigger = Input.GetAxis("Horizontal");
 
-            originalColour = GameObject.Find("WebcamRight").renderer.material.color;
-            GameObject.Find("WebcamRight").renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 0.0f);
+        if(A && !Btog) {
+            originalColour = WebcamLeft.renderer.material.color;
+            WebcamLeft.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 0.0f);
+
+            originalColour = WebcamRight.renderer.material.color;
+            WebcamRight.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 0.0f);
         }
-        else {
-            originalColour = GameObject.Find("WebcamLeft").renderer.material.color;
-            GameObject.Find("WebcamLeft").renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 1- (Input.GetAxis("Horizontal")));
+        else if (B) {
+            Btog = true;
 
-            originalColour = GameObject.Find("WebcamRight").renderer.material.color;
-            GameObject.Find("WebcamRight").renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 1- (Input.GetAxis("Horizontal")));
+            originalColour = WebcamLeft.renderer.material.color;
+            WebcamLeft.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, Mathf.Lerp(originalColour.a, 0.0f, 0.1f));
+
+            originalColour = WebcamRight.renderer.material.color;
+            WebcamRight.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, Mathf.Lerp(originalColour.a, 0.0f, 0.1f));
+        }
+        else if (!B && Btog) {
+            
+            WebcamLeft.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, Mathf.Lerp(WebcamLeft.renderer.material.color.a, 1.0f, 0.1f));
+
+            WebcamRight.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, Mathf.Lerp(WebcamRight.renderer.material.color.a, 1.0f, 0.1f));
+        }
+        else if (rTrigger != 0 && !Btog) {
+            originalColour = WebcamLeft.renderer.material.color;
+            WebcamLeft.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 1 - rTrigger);
+
+            originalColour = WebcamRight.renderer.material.color;
+            WebcamRight.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 1 - rTrigger);
+        }
+        else if (rTrigger == 0 && !Btog) {
+            originalColour = WebcamLeft.renderer.material.color;
+            WebcamLeft.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 1.0f);
+
+            originalColour = WebcamRight.renderer.material.color;
+            WebcamRight.renderer.material.color = new Color(originalColour.r, originalColour.g, originalColour.b, 1.0f);
+        }
+
+        if (Btog && WebcamLeft.renderer.material.color.a >= 0.95f && WebcamRight.renderer.material.color.a >= 0.95f) {
+            Btog = false;
         }
 
     }
 }
-
-//if (virt)
-//        {
-//            //Debug.Log("virtual is true");
-//            GameObject.Find("CameraLeft").camera.cullingMask = 1 << 30;
-//            GameObject.Find("CameraRight").camera.cullingMask = 1 << 31;
-//        }
-//        else
-//        {
-//            //Debug.Log("virtual is false");
-//            GameObject.Find("CameraLeft").camera.cullingMask = 1 << 0;
-//            GameObject.Find("CameraRight").camera.cullingMask = 1 << 0;
-//        }
-//}
